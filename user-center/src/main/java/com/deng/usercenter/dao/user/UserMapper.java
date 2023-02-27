6174f82fc8b611a02f7dd13e468d9afc604a7cff
deng <Dengqlbq@outlook.com>
2023-02-27 20:31:05 +0800

    [add] 整合mybatis通用mapper

---
+++ user-center/src/main/java/com/deng/usercenter/dao/user/UserMapper.java
@@ -0,0 +1,7 @@
+package com.deng.usercenter.dao.user;
+
+import com.deng.usercenter.domain.entity.user.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
